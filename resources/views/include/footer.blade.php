6568b8b77cd8c12b71cef46f635df5a5da0b3ac3
anggakahaerul <anggakahaerul20@gmail.com>
2019-01-12 22:57:49 +0700

    pangkas

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,69 +1,10 @@
 	<footer class="page-footer text-center text-md-left pt-4">
       <!--Footer Links-->
-      <div class="container-fluid">
-        <div class="row">
-          <!--First column-->
-          <div class="col-md-3 offset-md-1">
-            <h5 class="font-weight-bold text-uppercase mb-4">About material design</h5>
-            <p>Material Design (codenamed Quantum Paper) is a design language developed by Google. </p>
-            <p>Material Design for Bootstrap (MDB) is a powerful Material Design UI KIT for most popular HTML, CSS, and JS framework - Bootstrap.</p>
-          </div>
-          <!--/.First column-->
-          <hr class="clearfix w-100 d-md-none">
-          <!--Second column-->
-          <div class="col-md-2 offset-md-1">
-            <h5 class="font-weight-bold text-uppercase mb-4">First column</h5>
-            <ul class="list-unstyled">
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 1</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 2</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 3</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 4</a></li>
-            </ul>
-          </div>
-          <!--/.Second column-->
-          <hr class="clearfix w-100 d-md-none">
-          <!--Third column-->
-          <div class="col-md-2">
-            <h5 class="font-weight-bold text-uppercase mb-4">Second column</h5>
-            <ul class="list-unstyled">
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 1</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 2</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 3</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 4</a></li>
-            </ul>
-          </div>
-          <!--/.Third column-->
-          <hr class="clearfix w-100 d-md-none">
-          <!--Fourth column-->
-          <div class="col-md-2">
-            <h5 class="font-weight-bold text-uppercase mb-4">Third column</h5>
-            <ul class="list-unstyled">
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 1</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 2</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 3</a></li>
-              <li><a href="https://mdbootstrap.com/previews/docs/latest/html/carousel/video/index.html#!">Link 4</a></li>
-            </ul>
-          </div>
-          <!--/.Fourth column-->
-        </div>
-      </div>
-      <!--/.Footer Links-->
-      <hr>
-      <!--Call to action-->
-      <div class="call-to-action text-center">
-        <h4 class="my-4">Material Design for Bootstrap</h4>
-        <ul class="list-unstyled list-inline mb-4">
-          <li>
-            <h5 class="my-4">Get our UI KIT for free</h5></li>
-          <li class="list-inline-item"><a target="_blank" href="https://mdbootstrap.com/getting-started/" class="btn btn-info waves-effect waves-light">Sign up!</a></li>
-          <li class="list-inline-item"><a target="_blank" href="https://mdbootstrap.com/material-design-for-bootstrap/" class="btn btn-default waves-effect waves-light">Learn more</a></li>
-        </ul>
-      </div>
-      <!--/.Call to action-->
+
       <!--Copyright-->
       <div class="footer-copyright text-center">
         <div class="container-fluid py-3">
-          © 2018 Copyright: <a href="https://mdbootstrap.com/"> MDBootstrap.com </a>
+          © 2019 Copyright: Webthetics Team SMKN 13 Bandung</a>
         </div>
       </div>
       <!--/.Copyright-->
--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -95,11 +95,7 @@
         @include('include.menubar')
         @endif
 
-        <form class="form-inline">
-          <div class="md-form my-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          </div>
-        </form>
+       
       </div>
       <!-- Collapsible content -->
 
